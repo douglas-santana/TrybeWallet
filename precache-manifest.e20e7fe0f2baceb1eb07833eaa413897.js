80415cf3cb10669b492093e1d832303314495564
Douglas-Santana <douguinhodns@gmail.com>
2021-11-04 18:37:35 -0300

    Updates

---
+++ precache-manifest.e20e7fe0f2baceb1eb07833eaa413897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca64d84a5b26e03eb557f77bd98f059",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "ccca1d7b680743f926e9",
+    "url": "/trybewallet/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "8fdd3893981df304ae84",
+    "url": "/trybewallet/static/js/2.25aa3989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.25aa3989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccca1d7b680743f926e9",
+    "url": "/trybewallet/static/js/main.743dd2dc.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
